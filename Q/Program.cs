9895b881ab3026a3293fa9ade99591d89ccce156
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2020-09-24 11:37:46 +0200

    Add project files.

---
+++ Q/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue[] KoListe = new Queue[6];
+            for (int i = 0; i < KoListe.Length; i++)
+            {
+                KoListe[i] = new Queue();
+            }
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 600; i++)
+            {
+                int x = rnd.Next(1, 7) - 1;
+                KoListe[x].enQue(x); 
+            }
+            for (int i = 0; i < KoListe.Length; i++)
+            {
+                KoListe[i].printQue();
+            }    
+        }
+    }
+}
---
+++ Q/Queue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q
+{
+    class Queue
+    {
+        private int[] Que;
+        private int capacity = 1;
+        public Queue()
+            {
+            Que = new int[capacity];
+            }
+        public void enQue(int newObject)
+        {
+            if (Que.Length == capacity)
+            {
+                increaseLength();
+            }
+            int[] tempArray = Que;
+            Que = new int[capacity];
+            for (int i = 1; i < tempArray.Length; i++)
+            {
+                Que[i] = tempArray[i - 1];
+            }
+            Que[0] = newObject;
+
+        }
+        public void deQue()
+        {
+
+            if (capacity < 1)
+            {
+                throw new IndexOutOfRangeException("shits broke");
+            }
+                decreaseLength();
+
+            int[] tempArray = Que;
+            Que = new int[capacity];
+            
+            for (int i = 0; i < Que.Length; i++)
+            {
+                Que[i] = tempArray[i];
+            }
+        }
+        public void printQue()
+        {
+            Console.WriteLine(Que.Length);
+        }
+        private void increaseLength()
+        {
+            capacity++;
+        }
+        private void decreaseLength()
+        {
+            capacity--;
+        }
+    }
+}
